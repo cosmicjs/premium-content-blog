7e2105a879dd8e1c6815b1402c2669e5c6338a30
mateocain <msc66@pitt.edu>
2017-08-09 18:25:21 -0400

    API bug fixes

--- routes/api.js
+++ routes/api.js
@@ -4,15 +4,12 @@ var cosmic = require('cosmicjs')
 var axios = require('axios')
 
 router.post('/', function(req, res) {
-  var stripe = require('stripe')(req.app.locals.stripeKeySecret)
-  var endpointSecret = 'whsec_NEcZRxKQlHOQYgbuvWBoiX4UNhHADMXL'
-  var sig = req.headers['stripe-signature']
-  var event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret)
-  switch (req.body.type) {
+  var event = JSON.parse(req.body)
+  switch (event.type) {
     case 'charge.succeeded':
       cosmic.getObject(req.app.locals.config, { slug: 'subscriptions' }, function (err, response) {
         var currentObject = response.object
-        currentObject.metadata.revenue = currentObject.metadata.revenue + event.object.amount
+        currentObject.metadata.revenue = currentObject.metadata.revenue + event.data.object.amount
         axios({
           method: 'put',
           url: `https://api.cosmicjs.com/v1/${req.app.locals.config.bucket.slug}/edit-object`,
