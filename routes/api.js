f5c1338d6f03e71a86290c4b3f3679f4ddd071fb
mateocain <msc66@pitt.edu>
2017-08-09 18:04:08 -0400

    Syntax fix

--- routes/api.js
+++ routes/api.js
@@ -33,7 +33,7 @@ router.post('/', function(req, res) {
           })
         })
       })
-      res.status(200).json({ received: true)
+      res.status(200).json({ received: true})
     default:
       res.status(500).json({ error: 'Bad Request' })
       break;
